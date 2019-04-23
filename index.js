4432564a1202e23536d0cd8d4d450e305d787580
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-22 22:27:44 -0300

    Config express

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const http = require("http");
+const express = require("express");
 
-http
-  .createServer((req, res) => {
-    console.log(req);
-    return res.end("Hello World!");
-  })
-  .listen(3000);
+const app = express();
+
+app.get("/", (req, res) => {
+  // http://localhost:3000/?name=Rafael
+  return res.send(`Helloou, ${req.query.name}`);
+});
+
+app.get("/login", (req, res) => {
+  // http://localhost:3000/login
+  return res.send("Login");
+});
+
+app.get("/nome/:name", (req, res) => {
+  // http://localhost:3000/nome/Rafael
+  return res.json({
+    message: `Bem vindo, ${req.params.name}`
+  });
+});
+
+app.listen(3000);
