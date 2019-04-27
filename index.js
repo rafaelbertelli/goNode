0368e8d28a96f54d2ca96f0d9c6f2a387a081b25
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-26 23:31:57 -0300

    Nunjuck config

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
 
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
+app.set("view engine", "njk");
+
 app.get("/", (req, res) => {
   // http://localhost:3000
-  return res.send(`Helloou, bem vindo!`);
+  return res.render("list", { name: "Borgera" });
 });
 
 app.listen(3000);
