169ab169983f0ce204e92ffbb7c4f49e7aa685b7
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-26 22:06:46 -0300

    Nunjucks bootstrap

--- index.js
+++ index.js
@@ -2,33 +2,9 @@ const express = require("express");
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-  console.log(
-    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
-
-  req.appName = "GoNode";
-
-  return next();
-};
-
-app.use(logMiddleware);
-
-app.get("/", logMiddleware, (req, res) => {
-  // http://localhost:3000/?name=Rafael
-  return res.send(`Helloou, bem vindo ao ${req.appName}, ${req.query.name}`);
-});
-
-app.get("/login", (req, res) => {
-  // http://localhost:3000/login
-  return res.send("Login");
-});
-
-app.get("/nome/:name", (req, res) => {
-  // http://localhost:3000/nome/Rafael
-  return res.json({
-    message: `Bem vindo, ${req.params.name}`
-  });
+app.get("/", (req, res) => {
+  // http://localhost:3000
+  return res.send(`Helloou, bem vindo!`);
 });
 
 app.listen(3000);
